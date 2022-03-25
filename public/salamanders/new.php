ef0ce5ac578c28b0fbf721f41d186e42529e32c3
Ally Wilson <ally.steffen@gmail.com>
2022-03-25 15:12:49 -0400

    Update new file

--- public/salamanders/new.php
+++ public/salamanders/new.php
@@ -8,7 +8,48 @@ if ($test == '404') {
   error_404();
 } elseif ($test == '500') {
   error_500();
-} else {
-  echo 'No error';
+} elseif($test == 'redirect') {
+  redirect_to(url_for('/salamanders/index.php'));
 }
 ?>
+
+<?php $page_title = 'Create Subject'; ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
+
+<div id="content">
+
+  <a class="back-link" href="<?php echo url_for('/salamanders/index.php'); ?>">&laquo; Back to List</a>
+
+  <div class="subject new">
+    <h1>Create Salamander</h1>
+
+    <form action="" method="post">
+      <dl>
+        <dt>Menu Name</dt>
+        <dd><input type="text" name="menu_name" value="" /></dd>
+      </dl>
+      <dl>
+        <dt>Position</dt>
+        <dd>
+          <select name="position">
+            <option value="1">1</option>
+          </select>
+        </dd>
+      </dl>
+      <dl>
+        <dt>Visible</dt>
+        <dd>
+          <input type="hidden" name="visible" value="0" />
+          <input type="checkbox" name="visible" value="1" />
+        </dd>
+      </dl>
+      <div id="operations">
+        <input type="submit" value="Create Subject" />
+      </div>
+    </form>
+
+  </div>
+
+</div>
+
+<?php include(SHARED_PATH . '/staff_footer.php'); ?>
