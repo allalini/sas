cb2b8b2e81d5cb461d545a2234042145ff4474e3
allaleelee <ally.steffen@gmail.com>
2022-04-15 00:24:48 -0400

    updated db_credentials.php. Added a README file

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -4,10 +4,4 @@ define("DB_SERVER", "localhost");
 define("DB_USER", "sally");
 define("DB_PASS", "somepa55word");
 define("DB_NAME", "salamanders");
-
-// Don't commit this stuff!!!!!! >:()
-// define("DB_SERVER", "mysql.web182.thisweirdthing.fun");
-// define("DB_USER", "allisonwilson");
-// define("DB_PASS", "vFPP8Be5F3skNt34!!");
-// define("DB_NAME", "salamanders");
 ?>
