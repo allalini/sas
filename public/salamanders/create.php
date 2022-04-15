2ab50f104b25957e0fcdb02b91138876bf4e10b4
Ally Wilson <ally.steffen@gmail.com>
2022-04-14 23:30:03 -0400

    Modify create.php so the code passes an array

--- public/salamanders/create.php
+++ public/salamanders/create.php
@@ -6,11 +6,11 @@ if(is_post_request()) {
 
 // Handle form values sent by new.php
 
-$salamander_name = $_POST['salamander_name'] ?? '';
-$salamander_habitat = $_POST['salamander_habitat'] ?? '';
-$salamander_description = $_POST['salamander_description'] ?? '';
+$salamander ['name'] = $_POST['name'] ?? '';
+$salamander ['habitat'] = $_POST['habitat'] ?? '';
+$salamander ['description'] = $_POST['description'] ?? '';
 
-$result = insert_salamander($salamander_name, $salamander_habitat, $salamander_description);
+$result = insert_salamander($salamander);
 $new_id = mysqli_insert_id($db);
 redirect_to(url_for('salamanders/show.php?id=' . $new_id));
 
